22499e2f360137bc313d8a00c0bf0a9295b1904a
github-action-benchmark <github@users.noreply.github.com>
2025-11-16 10:50:14 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for f3a78be2226285ba84d6564b08a7e34507e1f8c6

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1763290214220,
+  "repoUrl": "https://github.com/barelooksrare/godot-solana-sdk",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "axel.benjaminsson@live.se",
+            "name": "Virus-Axel",
+            "username": "Virus-Axel"
+          },
+          "committer": {
+            "email": "axel.benjaminsson@live.se",
+            "name": "Virus-Axel",
+            "username": "Virus-Axel"
+          },
+          "distinct": true,
+          "id": "f3a78be2226285ba84d6564b08a7e34507e1f8c6",
+          "message": "Bump config version",
+          "timestamp": "2025-11-12T17:31:48+01:00",
+          "tree_id": "dd3a8c931bfb9c602efd05a4e32f577e2791ad84",
+          "url": "https://github.com/barelooksrare/godot-solana-sdk/commit/f3a78be2226285ba84d6564b08a7e34507e1f8c6"
+        },
+        "date": 1763290213776,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Simple Transaction Performance Index",
+            "value": 0.373287671232877,
+            "unit": "Percent"
+          }
+        ]
+      }
+    ]
+  }
+}
